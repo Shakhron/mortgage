539bbc29c9a650cbbe17a3919ac02f13558bf9aa
shahron <sokers30@gmail.cp>
2021-05-01 19:38:54 +0300

    MenuStart

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,27 @@
 package com.company;
 
-import java.util.Scanner
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        menu();
+    }
+    static void menu(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Menu: \n1)Calculate your martgage\n2)Exit");
+        int k = in.nextInt();
+        switch (k){
+            case 1:
+                calculate();
+            case 2:
+                break;
+            default:
+                System.out.println("Input Error");
+        }
 
     }
-    void menu(){
-    
+    static void calculate(){
+
     }
 }
