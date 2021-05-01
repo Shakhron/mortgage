30675bcd6be8d4c36277748bbe3c21d640bb8a28
shahron <sokers30@gmail.cp>
2021-05-01 20:14:56 +0300

    Class Mortgage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        menu();
+        System.out.println();
+        Mortgage ex = new Mortgage();
+        menu(ex);
     }
-    static void menu(){
+    static void menu(Mortgage cl){
         Scanner in = new Scanner(System.in);
-        System.out.println("Menu: \n1)Calculate your martgage\n2)Exit");
+        System.out.println("Menu: \n1)Calculate monthly payments\n2)Exit");
         int k = in.nextInt();
         switch (k){
             case 1:
-                calculate();
+                    cl.calculate();
             case 2:
                 break;
             default:
                 System.out.println("Input Error");
         }
-
-    }
-    static void calculate(){
 
     }
 }
--- src/com/company/Mortgage.java
+++ src/com/company/Mortgage.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class Mortgage {
+    int year;
+    int sum;
+    float percent;
+    Mortgage(int s, int y, float v){
+        this.sum = s;
+        this.year = y;
+        this.percent = v;
+    }
+    Mortgage(int s){
+        this(s,20,9.6f);
+    }
+    Mortgage(){
+        this.sum = 1500000;
+        this.year = 20;
+        this.percent = 9.6f;
+    }
+    private float Monthly_interest(){
+        return this.percent/12.0f/100.0f;
+    }
+    void calculate(){
+
+    }
 }
